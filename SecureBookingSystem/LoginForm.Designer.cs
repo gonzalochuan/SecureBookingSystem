42b88585349bcfa78c76d6a86cff12207274ec38
Gonzalo Chuan Jr <gonzalo.chuanjr@edufied.edu.vn>
2025-05-29 23:41:04 +0800

    Project Executed

--- SecureBookingSystem/LoginForm.Designer.cs
+++ SecureBookingSystem/LoginForm.Designer.cs
@@ -12,9 +12,13 @@
         private System.Windows.Forms.TextBox txtCaptcha;
         private System.Windows.Forms.Label lblCaptchaImage;
         private System.Windows.Forms.Label lblAttempts;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.lblUsername = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
             this.txtUsername = new System.Windows.Forms.TextBox();
@@ -24,91 +28,168 @@
             this.txtCaptcha = new System.Windows.Forms.TextBox();
             this.lblCaptchaImage = new System.Windows.Forms.Label();
             this.lblAttempts = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(30, 30);
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblUsername.Location = new System.Drawing.Point(50, 120);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(58, 13);
-            this.lblUsername.Text = "Username:";
+            this.lblUsername.Size = new System.Drawing.Size(67, 17);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(120, 27);
+            this.txtUsername.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.txtUsername.Location = new System.Drawing.Point(50, 140);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(150, 20);
+            this.txtUsername.Size = new System.Drawing.Size(250, 25);
+            this.txtUsername.TabIndex = 1;
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(30, 65);
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.lblPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblPassword.Location = new System.Drawing.Point(50, 180);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(56, 13);
-            this.lblPassword.Text = "Password:";
+            this.lblPassword.Size = new System.Drawing.Size(64, 17);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(120, 62);
+            this.txtPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.txtPassword.Location = new System.Drawing.Point(50, 200);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(150, 20);
-            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(250, 25);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.PasswordChar = '•';
             // 
             // lblCaptcha
             // 
             this.lblCaptcha.AutoSize = true;
-            this.lblCaptcha.Location = new System.Drawing.Point(30, 100);
+            this.lblCaptcha.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.lblCaptcha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblCaptcha.Location = new System.Drawing.Point(50, 240);
             this.lblCaptcha.Name = "lblCaptcha";
-            this.lblCaptcha.Size = new System.Drawing.Size(50, 13);
-            this.lblCaptcha.Text = "CAPTCHA:";
+            this.lblCaptcha.Size = new System.Drawing.Size(53, 17);
+            this.lblCaptcha.TabIndex = 4;
+            this.lblCaptcha.Text = "CAPTCHA";
             // 
             // lblCaptchaImage
             // 
-            this.lblCaptchaImage.AutoSize = true;
-            this.lblCaptchaImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.lblCaptchaImage.Location = new System.Drawing.Point(120, 95);
+            this.lblCaptchaImage.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblCaptchaImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCaptchaImage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaptchaImage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(182)))));
+            this.lblCaptchaImage.Location = new System.Drawing.Point(50, 260);
             this.lblCaptchaImage.Name = "lblCaptchaImage";
-            this.lblCaptchaImage.Size = new System.Drawing.Size(60, 20);
-            this.lblCaptchaImage.Text = "AB12C";
+            this.lblCaptchaImage.Size = new System.Drawing.Size(120, 30);
+            this.lblCaptchaImage.TabIndex = 5;
+            this.lblCaptchaImage.Text = "12345";
+            this.lblCaptchaImage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // txtCaptcha
             // 
-            this.txtCaptcha.Location = new System.Drawing.Point(120, 125);
+            this.txtCaptcha.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtCaptcha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCaptcha.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.txtCaptcha.Location = new System.Drawing.Point(180, 260);
             this.txtCaptcha.Name = "txtCaptcha";
-            this.txtCaptcha.Size = new System.Drawing.Size(150, 20);
+            this.txtCaptcha.Size = new System.Drawing.Size(120, 25);
+            this.txtCaptcha.TabIndex = 6;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(120, 160);
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(182)))));
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(50, 310);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(150, 30);
-            this.btnLogin.Text = "Login";
+            this.btnLogin.Size = new System.Drawing.Size(250, 35);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // lblAttempts
             // 
             this.lblAttempts.AutoSize = true;
-            this.lblAttempts.Location = new System.Drawing.Point(120, 200);
+            this.lblAttempts.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAttempts.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblAttempts.Location = new System.Drawing.Point(50, 360);
             this.lblAttempts.Name = "lblAttempts";
-            this.lblAttempts.Size = new System.Drawing.Size(0, 13);
+            this.lblAttempts.Size = new System.Drawing.Size(62, 13);
+            this.lblAttempts.TabIndex = 8;
             this.lblAttempts.Text = "Attempts: 0/5";
-            this.lblAttempts.ForeColor = System.Drawing.Color.Red;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(182)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 5);
+            this.panel1.TabIndex = 9;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblTitle.Location = new System.Drawing.Point(80, 40);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(190, 25);
+            this.lblTitle.TabIndex = 10;
+            this.lblTitle.Text = "Secure Booking Login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(50, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
             // 
             // LoginForm
             // 
-            this.ClientSize = new System.Drawing.Size(320, 240);
-            this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.lblCaptcha);
-            this.Controls.Add(this.lblCaptchaImage);
-            this.Controls.Add(this.txtCaptcha);
-            this.Controls.Add(this.btnLogin);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(350, 400);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtCaptcha);
+            this.Controls.Add(this.lblCaptchaImage);
+            this.Controls.Add(this.lblCaptcha);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "LoginForm";
-            this.Text = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Secure Booking System";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
